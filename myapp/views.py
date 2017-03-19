8da3db76a7cec709f078dbcd56f202840752a938
codephillip <codephillip@gmail.com>
2017-03-19 23:45:34 +0300

    removed duplicate code

--- myapp/views.py
+++ myapp/views.py
@@ -10,47 +10,28 @@ from myapp.serializers import MetricSerializer, UserSerializer, FeedbackSerializ
 
 @api_view(['GET', 'POST'])
 def metrics_route(request):
-    try:
-        metrics = Metric.objects.all()
-        print("metrics#")
-        print(metrics)
-    except Metric.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = MetricSerializer(metrics, many=True)
-        return Response({'metrics': serializer.data})
-
-    if request.method == 'POST':
-        print("posting#")
-        print(request.data)
-        serializer = MetricSerializer(data=request.data)
-        if serializer.is_valid():
-            print("Is valid")
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return master_route(request, 'metrics', Metric, MetricSerializer)
 
 
 @api_view(['GET', 'POST'])
 def feedbacks_route(request):
+    return master_route(request, 'feedbacks', Feedback, FeedbackSerializer)
+
+
+def master_route(request, tableName, Table, TableSerializer):
     try:
-        feedbacks = Feedback.objects.all()
-        print("feedbacks#")
-        print(feedbacks)
-    except Feedback.DoesNotExist:
+        tables = Table.objects.all()
+    except Table.DoesNotExist:
         return HttpResponse(status=404)
 
     if request.method == 'GET':
-        serializer = FeedbackSerializer(feedbacks, many=True)
-        return Response({'feedbacks': serializer.data})
+        return Response({tableName: TableSerializer(tables, many=True).data})
 
     if request.method == 'POST':
         print("posting#")
         print(request.data)
-        serializer = FeedbackSerializer(data=request.data)
+        serializer = TableSerializer(data=request.data)
         if serializer.is_valid():
-            print("Is valid")
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
