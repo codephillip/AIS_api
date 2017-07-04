b1fd44d6a94485bb6cef250140d97287fe8990a1
codephillip <codephillip@gmail.com>
2017-07-04 17:09:58 +0300

    launching to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn AIS_api.wsgi --log-file -
--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,254 @@ from rest_framework.response import Response
 from myapp.models import Metric, User, Feedback, Trigger, Crop
 from myapp.serializers import MetricSerializer, UserSerializer, FeedbackSerializer, TriggerSerializer, CropSerializer
 
+import urllib2
+import uuid
+import csv
+import json
+
+import pandas as pd
+from sklearn import metrics
+from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+
+percentage = 0.0
+final_classifier = 1
+
+
+def classifier1(X_train, X_test, y_train, y_test):
+    global percentage
+    global final_classifier
+    print("LogisticRegression")
+    # accuracy evaluation
+    classifier = LogisticRegression()
+    # fit the model to the training data (learn the coefficients)
+    classifier.fit(X_train, y_train)
+    # make predictions on the testing set
+    y_pred = classifier.predict(X_test)
+    print("y_test")
+    print(y_test)
+    print("y_pred")
+    print(y_pred)
+    temp_percentage = metrics.accuracy_score(y_test, y_pred)
+    if temp_percentage > percentage:
+        percentage = temp_percentage
+        final_classifier = 1
+    print("percentage: " + str(temp_percentage))
+    return classifier
+
+
+def classifier2(X_train, X_test, y_train, y_test):
+    global percentage
+    global final_classifier
+    print("LinearSVC")
+    # accuracy evaluation
+    classifier = LinearSVC()
+    # fit the model to the training data (learn the coefficients)
+    classifier.fit(X_train, y_train)
+    # make predictions on the testing set
+    y_pred = classifier.predict(X_test)
+    print("y_test")
+    print(y_test)
+    print("y_pred")
+    print(y_pred)
+    temp_percentage = metrics.accuracy_score(y_test, y_pred)
+    if temp_percentage > percentage:
+        percentage = temp_percentage
+        final_classifier = 2
+    print("percentage: " + str(temp_percentage))
+    return classifier
+
+
+def classifier3(X_train, X_test, y_train, y_test):
+    global percentage
+    global final_classifier
+    print("RandomForestClassifier")
+    # accuracy evaluation
+    classifier = RandomForestClassifier(n_estimators=3, max_depth=2)
+    # fit the model to the training data (learn the coefficients)
+    classifier.fit(X_train, y_train)
+    # make predictions on the testing set
+    y_pred = classifier.predict(X_test)
+    print("y_test")
+    print(y_test)
+    print("y_pred")
+    print(y_pred)
+    temp_percentage = metrics.accuracy_score(y_test, y_pred)
+    if temp_percentage > percentage:
+        percentage = temp_percentage
+        final_classifier = 3
+    print("percentage: " + str(temp_percentage))
+    return classifier
+
+
+def classifier4(X_train, X_test, y_train, y_test):
+    global percentage
+    global final_classifier
+    print("KNeighborsClassifier")
+    # accuracy evaluation
+    classifier = KNeighborsClassifier(n_neighbors=1)
+    # fit the model to the training data (learn the coefficients)
+    classifier.fit(X_train, y_train)
+    # make predictions on the testing set
+    y_pred = classifier.predict(X_test)
+    print("y_test")
+    print(y_test)
+    print("y_pred")
+    print(y_pred)
+    # compare the predicted values-y_pred, with the actual values-y_test
+    temp_percentage = metrics.accuracy_score(y_test, y_pred)
+    if temp_percentage > percentage:
+        percentage = temp_percentage
+        final_classifier = 4
+    print("percentage: " + str(temp_percentage))
+    return classifier
+
+
+def get_byte():
+    if int(str(uuid.uuid4().get_time())[0:3]) > 500:
+        return 0
+    else:
+        return 1
+
+
+def initialise():
+    csv_connection = open("/home/codephillip/PycharmProjects/ais_ml_algorithm/src/test.csv", "wb")
+    f = csv.writer(csv_connection)
+    # smv = soil moisture value. comes from the smv sensor on the hardware
+    f.writerow(["pk", "dt", "name", "temp", "humidity", "smv", "trigger"])
+
+    # production url
+    # url = "http://api.openweathermap.org" + "/data/2.5/forecast?id=" + str(
+    #     232422) + "&mode=json&units=metric&cnt=7&appid=1f846e7a0e00cf8c2f96dd5e768580fb"
+    # development url
+    url = "http://127.0.0.1:8080/weather1.json"
+    print(url)
+    # 'load'-for json document, 'loads'-for json string
+    x = json.load(urllib2.urlopen(url))
+    print(x)
+    city = x.get('city')
+
+    count = 0
+    for list_data in x.get('list'):
+        f.writerow([count + 1, list_data["dt_txt"],
+                    city["name"], list_data["main"]["temp"], list_data["main"]["humidity"],
+                    str(uuid.uuid4().get_node())[0:3],
+                    get_byte()])
+        count += 1
+    csv_connection.close()
+
+    csv_url = "/home/codephillip/PycharmProjects/ais_ml_algorithm/src/test.csv"
+    data = pd.read_csv(csv_url, index_col=0)
+    feature_cols = ['temp', 'humidity', 'smv']
+
+    print("ACTUAL DATA")
+    print(data)
+
+    X = data[feature_cols]
+    print(X.head())
+    print(type(X))
+    print(X.shape)
+
+    y = data['trigger']
+    print(y.head())
+    print(type(y))
+    print(y.shape)
+
+    # MACHINE LEARNING
+    # split the data into training and testing data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+    # default split is 75% for training and 25% for testing
+    print("train test split shapes")
+    print(X_train.shape)
+    print(y_train.shape)
+    print(X_test.shape)
+    print(y_test.shape)
+
+    # run all classifier to determine the best
+    print("running classifier1")
+    classifier_model1 = classifier1(X_train, X_test, y_train, y_test)
+    print("running classifier2")
+    classifier_model2 = classifier2(X_train, X_test, y_train, y_test)
+    print("running classifier3")
+    classifier_model3 = classifier3(X_train, X_test, y_train, y_test)
+    print("running classifier4")
+    classifier_model4 = classifier4(X_train, X_test, y_train, y_test)
+
+    # rerun the best on the data
+    csv_connection = open("/home/codephillip/PycharmProjects/ais_ml_algorithm/src/test.csv", "wb")
+    f = csv.writer(csv_connection)
+    # smv = soil moisture value. comes from the smv sensor on the hardware
+    f.writerow(["pk", "dt", "name", "temp", "humidity", "smv", "trigger"])
+
+    # todo capture new data
+    # production url
+    url = "http://api.openweathermap.org" + "/data/2.5/forecast?id=" + str(
+        232422) + "&mode=json&units=metric&cnt=21&appid=1f846e7a0e00cf8c2f96dd5e768580fb"
+    # development url
+    # url = "http://127.0.0.1:8080/weather1.json"
+    print(url)
+    # 'load'-for json document, 'loads'-for json string
+    x = json.load(urllib2.urlopen(url))
+    print(x)
+    city = x.get('city')
+
+    count = 1
+    for list_data in x.get('list'):
+        f.writerow([count, list_data["dt_txt"],
+                    city["name"], list_data["main"]["temp"], list_data["main"]["humidity"],
+                    str(uuid.uuid4().get_node())[0:3],
+                    0])
+        count += 1
+    csv_connection.close()
+
+    # csv_url = "http://127.0.0.1:8080/weather.csv"
+    # todo point to a global source for the csv file
+    csv_url = "/home/codephillip/PycharmProjects/ais_ml_algorithm/src/test.csv"
+    data = pd.read_csv(csv_url, index_col=0)
+    feature_cols = ['temp', 'humidity', 'smv']
+
+    print("ACTUAL DATA")
+    print(data)
+
+    X = data[feature_cols]
+    print(X.head())
+    print(type(X))
+    print(X.shape)
+
+    # choose best model and predict
+    if final_classifier == 1:
+        y = classifier_model1.predict(X)
+        print("FINAL PREDICTION: model1")
+        print(y)
+    elif final_classifier == 2:
+        y = classifier_model2.predict(X)
+        print("FINAL PREDICTION: model2")
+        print(y)
+    elif final_classifier == 3:
+        y = classifier_model3.predict(X)
+        print("FINAL PREDICTION: model3")
+        print(y)
+    elif final_classifier == 4:
+        y = classifier_model4.predict(X)
+        print("FINAL PREDICTION: model4")
+        print(y)
+
+    # save the triggers
+    csv_connection = open("/home/codephillip/PycharmProjects/ais_ml_algorithm/src/test.csv", "wb")
+    f = csv.writer(csv_connection)
+    print("saving data...")
+    count = 1
+    for list_data in x.get('list'):
+        f.writerow([count, list_data["dt_txt"],
+                    city["name"], list_data["main"]["temp"], list_data["main"]["humidity"],
+                    str(uuid.uuid4().get_node())[0:3],
+                    y[count - 1]])
+        count += 1
+    csv_connection.close()
+
 
 @api_view(['GET', 'POST'])
 def metrics_route(request):
@@ -17,8 +265,10 @@ def feedbacks_route(request):
     return master_route(request, 'feedbacks', Feedback, FeedbackSerializer)
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT'])
 def triggers_route(request):
+    if request.method == 'PUT':
+        initialise()
     return master_route(request, 'triggers', Trigger, TriggerSerializer)
 
 
